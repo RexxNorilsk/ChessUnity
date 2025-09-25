4866a630ec99fffdc6e8b72201ab8e5dd23533fd
RexxNorilsk <staxeev_ar@garpix.com>
2025-09-25 12:09:41 +0300

    Добавлены новые префабы для фигур шахматной игры, включая черные и белые фигуры. Обновлены метафайлы с правильными путями к префабам. Удалены ненужные префабы белых фигур. В классе Figure добавлены новые поля для хранения префабов фигур и их типов.

--- Assets/Scripts/Entities/Base/Figure.cs
+++ Assets/Scripts/Entities/Base/Figure.cs
@@ -3,6 +3,24 @@ using System.Collections.Generic;
 
 namespace Entities.Base {
     public class Figure : MonoBehaviour {
-            public PositionOnGrid Position;
+        public PositionOnGrid Position;
+        [SerializeField]
+        public List<FigurePrefab> FiguresPrefabs = new List<FigurePrefab>();
+
+        [System.Serializable]
+        public class FigurePrefab {
+            public FigureType type;
+            public Mesh prefab;
+        }
+
+        [System.Serializable]
+        public enum FigureType {
+            Pawn,
+            Rook,
+            Queen,
+            Elephant,
+            King,
+            Horse,
+        }
     }
 }
